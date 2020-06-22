4417e313ed112778f0df2c1f37b66076f4c185fd
Steven Marshall <SMarshall@chef.io>
2020-06-22 17:41:36 +0100

    [libiconv] Various refactors of existing code
    Add inspec.lock to .gitignore
    Update README.md to the latest library format
    Added plan_name to attributes.yml file
    Update control description, added stderr check and prescriptive version check
    Updated inspec.yml to latest format
    
    Signed-off-by: Steven Marshall <SMarshall@chef.io>

--- controls/libiconv_exists.rb
+++ controls/libiconv_exists.rb
@@ -8,24 +8,33 @@ control 'core-plans-libiconv' do
   impact 1.0
   title 'Ensure libiconv exists and works as expected'
   desc '
-  We check that the libiconv binary exists and checks the version to ensure it works.
+  To test that the iconv binary that libiconv creates works as expected we first get the path that it is installed at.
+  This path is then used to existance of and the execution of the iconv binary to ensure that it both exists and works as expected with the correct libiconv version installed.
+    $ $PKG_PATH/bin/inconv --version
+      iconv (GNU libiconv 1.14)
+      Copyright (C) 2000-2011 Free Software Foundation, Inc.
+      License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+      This is free software: you are free to change and redistribute it.
+      There is NO WARRANTY, to the extent permitted by law.
+      Written by Bruno Haible.
   '
 
   hab_pkg_path = command("hab pkg path #{plan_ident}")
   describe hab_pkg_path do
-    its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
+    its('stderr') { should be_empty }
+    its('exit_status') { should eq 0 }
   end
 
   describe command("ls #{File.join(hab_pkg_path.stdout.strip, "bin/iconv")}") do
     its('stdout') { should_not be_empty }
-    its('stderr') { should eq '' }
+    its('stderr') { should be_empty }
     its('exit_status') { should eq 0 }
   end
 
   describe command("#{File.join(hab_pkg_path.stdout.strip, "bin/iconv")} --version") do
-    its('stdout') { should match /iconv \(GNU libiconv [0-9]+.[0-9]+\)/ }
-    its('stderr') { should eq '' }
+    its('stdout') { should match /iconv \(GNU libiconv #{hab_pkg_path.stdout.strip.split('/')[5]}\)/ }
+    its('stderr') { should be_empty }
     its('exit_status') { should eq 0 }
   end
 end
