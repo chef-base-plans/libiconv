a6a9439b28d9ec4a86a3bddb5de3dda5c06c3d7b
Steven Marshall <SMarshall@chef.io>
2020-08-26 23:06:38 +0100

    [libiconv] Update to 1.16
    Signed-off-by: Steven Marshall <SMarshall@chef.io>

--- plan.sh
+++ plan.sh
@@ -1,5 +1,5 @@
 pkg_name=libiconv
-pkg_version=1.14
+pkg_version=1.16
 pkg_origin=core
 pkg_license=('GPL-3.0-or-later')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
@@ -13,7 +13,7 @@ and external string representation (a traditional encoding) when they \
 are doing I/O.  GNU libiconv is a conversion library for both kinds of \
 applications."
 pkg_filename="${pkg_name}"-"${pkg_version}".tar.gz
-pkg_shasum=72b24ded17d687193c3366d0ebe7cde1e6b18f0df8c55438ac95be39e8a30613
+pkg_shasum=e6a1b1b589654277ee790cce3734f07876ac4ccfaecbee8afa0b649cf529cc04
 pkg_deps=(core/glibc)
 pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc)
 pkg_lib_dirs=(lib)
