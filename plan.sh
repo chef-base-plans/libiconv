cdfe3113bb0b50d2d850a199ee87d621a21fcb96
Sangameshwar Mandakanalli <smandaka@progress.com>
2021-03-30 17:27:59 +0530

    Patching of old file removed

--- plan.sh
+++ plan.sh
@@ -20,7 +20,6 @@ pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
 
 do_build() {
-    patch -p1 -i "$PLAN_CONTEXT"/patches/libiconv-1.14_srclib_stdio.in.h-remove-gets-declarations.patch
     ./configure --prefix="${pkg_prefix}"
     make
 }
